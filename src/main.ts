59bcd7d70ef61eda2ceda6f2cf41384c4d6329f7
kurrorro <keishavnia@gmail.com>
2026-08-12 23:30:20 +0700

    feat: setup swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,37 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const apiPrefix = process.env.API_V1_PREFIX || '/api/v1';
+  app.setGlobalPrefix(apiPrefix, {
+    exclude: ['health', ''],
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('Sync Without Signal API')
+    .setDescription('The API documentation for K-POS Backend')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  // 2. Setup CORS
+  const corsOrigins = process.env.CORS_ORIGINS || '*';
+  app.enableCors({
+    origin: corsOrigins === '*' ? true : corsOrigins.split(','),
+    credentials: true,
+  });
+
+  // 3. Setup Port
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  
+  console.log(`Application is running on: http://localhost:${port}`);
+  console.log(`Swagger documentation is available at: http://localhost:${port}/docs`);
 }
 bootstrap().catch((err) => {
   console.error('Error during bootstrap:', err);
