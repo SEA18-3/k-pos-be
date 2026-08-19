f92d7efa5f1b33d4c97d9cd2c2b0ae706f85782c
kurrorro <keishavnia@gmail.com>
2026-08-19 10:47:57 +0700

    chore: remove accidentally tracked generated files

--- test-prisma.js
+++
@@ -1,9 +0,0 @@
-const { PrismaClient } = require('./generated/prisma/client');
-const prisma = new PrismaClient();
-async function test() {
-  const txs = await prisma.transaction.findMany();
-  console.log('Transactions:', txs.length);
-  const sq = await prisma.syncQueue.findMany();
-  console.log('SyncQueue:', sq.length, sq);
-}
-test().then(() => prisma.$disconnect());
